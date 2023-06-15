c1e19d97ba6fa9cdd28472408f891ce7cab06981
YunKukPark <a01043340999@gmail.com>
2023-06-15 22:49:40 +0900

    ✨modify: Hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,31 +1,32 @@
 'use client';
-import React, { useEffect, useRef } from 'react';
+
+import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
+import { dohyeon } from '@/app/fonts';
+import css from './Hero.module.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const FRAME = {
-  rate: 30,
-  total: 70,
-  scrollPerFrame: 40,
-};
-
 const STEPS = [
-  '☀️',
-  '여름',
-  '🎹',
-  '하지',
   '☀️여름 🎹하지',
   '🏖️ MIDNIGHT',
-  '🎏 SUMMER FESTIVAL',
+  '🎏 SUMMER',
+  '',
+  '',
   '✨ 지은진',
   '🌊🐦 물수제비',
   '👘🤯 버르장머리',
+  '',
+  '',
   '🎉 2023.06.24',
 ];
 
 const Hero = () => {
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  const isClient = typeof window !== 'undefined';
+  const stepWrapperRef = useRef<HTMLDivElement>(null);
   const stepRefs = useRef<HTMLDivElement[]>([]);
 
   useEffect(() => {
@@ -36,8 +37,6 @@ const Hero = () => {
         {
           scrollTrigger: {
             trigger: step,
-            start: '-=100 center',
-            end: '+=200 center',
             scrub: true,
             toggleActions: 'play none none reverse', // This line controls the fade in and out
           },
@@ -48,7 +47,49 @@ const Hero = () => {
     });
   }, []);
 
-  return <section className="w-screen"></section>;
+  const handleResize = () =>
+    setScrollHeight(
+      (stepWrapperRef.current?.clientHeight ?? 3000) + window.innerHeight
+    );
+
+  useEffect(() => {
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    // Clean up function
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <section className="w-screen">
+      <div className={css.videoContainer}>
+        <div
+          className={css.videoWrapper}
+          style={isClient ? { height: scrollHeight } : undefined}
+        >
+          <video
+            className={css.summerFestivalVideo}
+            src="./assets/midsummer.mp4"
+            autoPlay
+            loop
+            muted
+          />
+          <div className={`${css.stepWrapper} gap-16`} ref={stepWrapperRef}>
+            {STEPS.map((step, i) => (
+              <h2
+                key={step}
+                ref={el => (stepRefs.current[i] = el!)}
+                className={`${dohyeon.className} ${css.heroText}`}
+              >
+                {step}
+              </h2>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
